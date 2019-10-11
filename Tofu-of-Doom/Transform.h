269bda8ba2005cb7330117acdacf0f5cd3d90f3a
PaulJame5 <43470494+PaulJame5@users.noreply.github.com>
2019-10-11 19:37:10 +0100

    Add files via upload
    Fixed memory leak when drawing objects, also separated camera class and made a transform class for storing object positions in the x,y ,z plane and made a static function for finding distance of two objects in 3d space

---
+++ Tofu-of-Doom/Transform.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <cmath>
+
+
+	class Transform
+	{
+		typedef struct Position
+		{
+			float x = 0;
+			float y = 0;
+			float z = 0;
+		};
+
+	public:
+		static float distance(Position& pos_1, Position& pos_2);
+
+		Position position;
+
+		Transform();
+		~Transform();
+		
+
+	};
+
+	
