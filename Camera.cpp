b364fcd238b1c323f362470ce79fc3c228413087
PaulJame5 <43470494+PaulJame5@users.noreply.github.com>
2019-10-15 17:28:56 +0100

    Add files via upload
    Beginnings of controller support added

---
+++ Camera.cpp
@@ -0,0 +1,199 @@
+#include "Camera.h"
+
+
+glm::mat4 Camera::camera(glm::vec3 t_eye, double t_pitch, double t_yaw)
+{
+	double cosPitch = cos(glm::radians(t_pitch));
+	double sinPitch = sin(glm::radians(t_pitch));
+	double cosYaw = cos(glm::radians(t_yaw));
+	double sinYaw = sin(glm::radians(t_yaw));
+
+	glm::vec3 xaxis = { cosYaw, 0, -sinYaw };
+	glm::vec3 yaxis = { sinYaw * sinPitch, cosPitch, cosYaw * sinPitch };
+	glm::vec3 zaxis = { sinYaw * cosPitch, -sinPitch, cosPitch * cosYaw };
+
+	// Create a 4x4 view matrix from the right, up, forward and eye position vectors
+	glm::mat4 viewMatrix =
+	{
+		glm::vec4(xaxis.x,            yaxis.x,            zaxis.x,      0),
+		glm::vec4(xaxis.y,            yaxis.y,            zaxis.y,      0),
+		glm::vec4(xaxis.z,            yaxis.z,            zaxis.z,      0),
+		glm::vec4(-dot(xaxis, m_eye), -dot(yaxis, m_eye), -dot(zaxis, m_eye), 1)
+	};
+
+	return viewMatrix;
+}
+
+glm::vec3 Camera::getEye()
+{
+	return m_eye;
+}
+
+void Camera::setEye(glm::vec3 t_values)
+{
+	m_eye = t_values;
+}
+
+glm::mat4 Camera::getView()
+{
+	return camera(m_eye, m_pitch, m_yaw);
+}
+
+glm::vec4 Camera::getDirection()
+{
+	return m_direction;
+}
+
+float Camera::getSpeed()
+{
+	return m_speed;
+}
+
+Camera::Camera()
+{
+}
+
+Camera::~Camera()
+{
+}
+
+void Camera::input(sf::Time t_deltaTime)
+{
+	Player1 = new CXBOXController(1);
+	if (Player1->IsConnected())
+	{
+		if (Player1->GetState().Gamepad.wButtons & XINPUT_GAMEPAD_A)
+		{
+			Player1->Vibrate(65535, 0);
+		}
+
+		if (Player1->GetState().Gamepad.wButtons & XINPUT_GAMEPAD_B)
+		{
+			Player1->Vibrate(0, 65535);
+		}
+
+		if (Player1->GetState().Gamepad.wButtons & XINPUT_GAMEPAD_X)
+		{
+			Player1->Vibrate(65535, 65535);
+		}
+
+		if (Player1->GetState().Gamepad.wButtons & XINPUT_GAMEPAD_Y)
+		{
+			Player1->Vibrate();
+		}
+
+		if (Player1->GetState().Gamepad.wButtons & XINPUT_GAMEPAD_BACK)
+		{
+		}
+
+		if (Player1->GetState().Gamepad.sThumbLY > 7849)
+		{
+			glm::vec3 tempDirection(m_direction.x, m_direction.y, m_direction.z);
+			glm::normalize(tempDirection);
+			m_eye -= tempDirection * static_cast<float>(t_deltaTime.asMilliseconds() * m_speed);
+
+		}
+		else if (Player1->GetState().Gamepad.sThumbLY < -7849)
+		{
+			glm::vec3 tempDirection(m_direction.x, m_direction.y, m_direction.z);
+			glm::normalize(tempDirection);
+			m_eye += tempDirection * static_cast<float>(t_deltaTime.asMilliseconds() * m_speed);
+		}
+
+		if (Player1->GetState().Gamepad.sThumbRX < -7849)
+		{
+			m_yaw += 2.0;
+			m_rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(-2.0f), glm::vec3(0.f, 1.f, 0.f));
+			m_direction = m_direction * m_rotationMatrix;
+
+			if (m_yaw >= 360.0)
+			{
+				m_yaw = 0.0;
+				m_direction = glm::vec4(0.f, 0.f, 1.f, 0.f);
+			}
+		}
+		else if (Player1->GetState().Gamepad.sThumbRX > 7849)
+		{
+			m_yaw -= 2.0;
+			m_rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(2.0f), glm::vec3(0.f, 1.f, 0.f));
+			m_direction = m_direction * m_rotationMatrix;
+
+			if (m_yaw <= -360.0)
+			{
+				m_yaw = 0.0;
+				m_direction = glm::vec4(0.f, 0.f, 1.f, 0.f);
+			}
+		}
+		delete(Player1);
+	}
+	/*if (control.aButton())
+	{
+
+	}*/
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+	{
+		glm::vec3 tempDirection(m_direction.x, m_direction.y, m_direction.z);
+		glm::normalize(tempDirection);
+		m_eye -= tempDirection * static_cast<float>(t_deltaTime.asMilliseconds() * m_speed);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		glm::vec3 tempDirection(m_direction.x, m_direction.y, m_direction.z);
+		glm::normalize(tempDirection);
+		m_eye += tempDirection * static_cast<float>(t_deltaTime.asMilliseconds() * m_speed);
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		m_eye.x -= .1f;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		m_eye.x += .1f;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::N))
+	{
+		m_yaw += 2.0;
+		m_rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(-2.0f), glm::vec3(0.f, 1.f, 0.f));
+		m_direction = m_direction * m_rotationMatrix;
+
+		if (m_yaw >= 360.0)
+		{
+			m_yaw = 0.0;
+			m_direction = glm::vec4(0.f, 0.f, 1.f, 0.f);
+		}
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::M))
+	{
+		m_yaw -= 2.0;
+		m_rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(2.0f), glm::vec3(0.f, 1.f, 0.f));
+		m_direction = m_direction * m_rotationMatrix;
+
+		if (m_yaw <= -360.0)
+		{
+			m_yaw = 0.0;
+			m_direction = glm::vec4(0.f, 0.f, 1.f, 0.f);
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		m_pitch += 1.0;
+
+		if (m_pitch >= 360.0)
+		{
+			m_pitch = 0.0;
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		m_pitch -= 1.0;
+
+		if (m_pitch <= -360.0)
+		{
+			m_pitch = 0.0;
+		}
+	}
+}
---
+++ Room.cpp
@@ -0,0 +1,78 @@
+#include "Room.h"
+#include <string>
+void Room::initialise()
+{
+	if (!textureValuesLoaded)
+	{
+
+
+		// Load texture
+	//	filename = "cottage-texture.png";
+		//	stbi_set_flip_vertically_on_load(false);
+		texture_data = stbi_load(filename.c_str(), &width, &height, &comp_count, 3);
+		textureValuesLoaded = true;
+	}
+	glGenTextures(1, &Texture);
+	glBindTexture(GL_TEXTURE_2D, Texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_data);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	bool res = m_modelLoader.loadOBJ("cottage.obj", vertices, uvs, normals);
+	verticesInit = vertices;
+
+	GLuint m_error = glewInit(); // Initialise GLEW
+
+	// Load vertex and fragment shader files into shader objects
+	m_genericShaderRoom = new tk::Shader("Standard.vert", "Standard.frag");
+
+	GLint isCompiled = 0;
+	GLint isLinked = 0;
+
+
+	// Get a handle for our "myTextureSampler" uniform
+	TextureID = glGetUniformLocation(m_genericShaderRoom->m_programID, "myTextureSampler");
+	loaded += 1;
+	std::cout << "Loaded " << std::to_string(loaded) << "%" << std::endl;
+}
+
+std::vector<glm::vec3> Room::getVertices()
+{
+	return vertices;
+}
+
+std::vector<glm::vec2> Room::getUvs()
+{
+	return uvs;
+}
+
+std::vector<glm::vec3> Room::getNormals()
+{
+	return normals;
+}
+
+
+
+GLuint Room::getVertexBuffer()
+{
+	return vertexbuffer;
+}
+
+void Room::setPosition()
+{
+	for (int i = 0; i < vertices.size(); i++)
+	{
+		vertices[i].x = verticesInit[i].x + transform.position.x;
+		vertices[i].y = verticesInit[i].y + transform.position.y;
+		vertices[i].z = verticesInit[i].z + transform.position.z;
+	}
+}
+
+Room::Room()
+{
+}
+
+Room::~Room()
+{
+}
