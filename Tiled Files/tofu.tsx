93ecffe238dd7f566e30b406ef30b003545392e0
alan-bolger <alan.j.bolger@gmail.com>
2020-02-15 19:56:17 +0000

    Added Tiled map stuff.

---
+++ Tiled Files/tofu.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tofu" tilewidth="50" tileheight="50" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="4">
+  <image width="50" height="50" source="walltype_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="50" height="50" source="walltype_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="50" height="50" source="floor.png"/>
+ </tile>
+ <tile id="7">
+  <image width="50" height="50" source="light.png"/>
+ </tile>
+</tileset>
